52e1eb3be331768ad51780033c38a887c7c23a7a
Walter <wrkagel@uw.edu>
2022-01-05 14:31:46 -0800

    Made login catchall page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import EmployeePage from "./components/employee-page";
 import Login from "./components/login";
 import ManagerPage from "./components/manager-page";
@@ -10,8 +11,20 @@ function App() {
 
   return (<>
     <h1>Reimbursement System</h1>
-    <Login />
-    {user.isAuthenticated && (user.isManager ? <ManagerPage /> : <EmployeePage />)}
+    <BrowserRouter>
+    <Routes>
+    {user.isAuthenticated ? 
+      <>      
+      <Route path='/employee' element={<EmployeePage />}/>
+      <Route path='/manager' element={<ManagerPage />}/>
+      </>
+      :
+      <>
+      <Route path='/login' element={<Login />}/>
+      <Route path='*' element={<Navigate to={"/login"}/>}/>
+      </>}
+    </Routes>
+    </BrowserRouter>
   </>);
 }
 
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,6 +1,7 @@
 import { Dispatch } from "@reduxjs/toolkit";
 import { useRef } from "react"
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import User from "../models/user";
 import { actions } from "../store";
 
@@ -10,11 +11,14 @@ export default function Login() {
     const nameInput = useRef<HTMLInputElement>(null);
     const passInput = useRef<HTMLInputElement>(null);
     const dispatch = useDispatch<Dispatch<{type:string, payload:User}>>();
+    const navigate = useNavigate();
 
     function validateUser() {
         //Dummy code to be fixed after Backend created
-        const action = actions.updateUser({isAuthenticated:true, id:'007', isManager:false});
+        const dummyUser:User = {isAuthenticated:true, id:'007', isManager:false}
+        const action = actions.updateUser(dummyUser);
         dispatch(action);
+        navigate(dummyUser.isManager ? '/manager' : '/employee');
     }
 
     return (<>
