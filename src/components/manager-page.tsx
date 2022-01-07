cbafe5c172c21552e95205c170568f90d7321288
Walter <wrkagel@uw.edu>
2022-01-07 14:19:11 -0800

    Working drop down menu for manager selecting employee

--- src/components/manager-page.tsx
+++ src/components/manager-page.tsx
@@ -1,17 +1,56 @@
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
-import ManagerNavBar from "./manager-nav-bar";
+import Employee from "../entities/employee";
+import ReimbursementItem from "../entities/reimbursement-item";
+import { actions, PageState } from "../store";
 import ReimbursementTable from "./reimbursement-table";
 import StatisticsPage from "./statistics-page";
 
 
 export default function ManagerPage() {
 
+    const id = useSelector((state:PageState) => state.user.id);
+    const dispatch = useDispatch();
+
+    const [employees, setEmployees] = useState<Employee[]>([]);
+    const employeeInput = useRef<HTMLSelectElement>(null);
+    useEffect(() => {getManages()}, []);
+
+    async function getManages() {    
+        
+        const response = await fetch(`http://localhost:5000/employees/${id}`);
+        const {manages} = await response.json();
+        const response2 = await fetch('http://localhost:5000/employees/managed', {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(manages)
+        })
+        const employees:Employee[] = await response2.json();
+        setEmployees(employees);
+        const action = actions.updateReimbursementList([]);
+        dispatch(action);
+    }
+
+    async function updateReimbursementList() {
+        const id:string = employeeInput.current?.value ?? "";
+        if(!id) return;
+        const response = await fetch(`http://localhost:5000/reimbursements/${id}`);
+        const reimbursements:ReimbursementItem[] = await response.json();
+        const action = actions.updateReimbursementList(reimbursements);
+        dispatch(action);
+    }
+
     return (<>
         <h3>You are on the Manager Page</h3>
         <Routes>
             <Route path={'/statistics'} element={<StatisticsPage />}/>
             <Route path={'/reimbursements'} element={<>
-                <select className="" id=""></select>
+                <label htmlFor="employeeInput">Choose an Employee</label>
+                <select defaultValue={""} onChange={updateReimbursementList} ref={employeeInput} id="employeeInput">
+                    <option value="">Please choose an option</option>
+                    {employees.map(e => <option key={e.id} value={e.id}>{e.fname + e.mname + e.lname}</option>)}
+                </select>
                 <ReimbursementTable />
             </>}>
             </Route>
