1d4823eb2460fd518473bbde109eeed01bc8a9c9
Walter <wrkagel@uw.edu>
2022-01-09 15:07:02 -0800

    Testing switch to https

--- src/index.tsx
+++ src/index.tsx
@@ -14,4 +14,4 @@ ReactDOM.render(
 );
 
 //export const backendAddress = 'http://localhost:47477';
-export const backendAddress = 'http://wrkagel-revature-project1.eastus.cloudapp.azure.com:47477'
+export const backendAddress = 'https://wrkagel-revature-project1.eastus.cloudapp.azure.com:47477'
